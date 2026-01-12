99085f838fd8e754c9ea4874ff1c6aad47351703
Unexpectedbox <jdelrosario5750@gmail.com>
2026-01-12 05:15:29 -0800

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <Iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "My Name is Jonathan DelRosario" << endl;
+	cout << "My Goal in life is to live a life with no financial or external worries." << endl;
+	cout << "My current intrest are card games such as Magic the Gathering and Riftbound. Also enjoy video games as well." << endl;
+	cout << "My dream is to never have to look at a price tag and have to way the pro and cons of an item if I like it." << endl;
+	cout << "I expect to see myself in a career that is climbable in 5 years and having the ability to endulge in my lifestyle." << endl;
+
+	return 0;
+
+}
